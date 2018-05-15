aa45513263ffd85549c104d6117bbff414f35b42
machester <shusheng1991@gmail.com>
2018-05-15 13:26:36 +0800

    1. twosum

--- 1.twoSum/twosum.c
+++ 1.twoSum/twosum.c
@@ -8,21 +8,21 @@ int* twoSum(int* nums, int numsSize, int target) {
 	int find = 0;
 	int loop1;
 	int loop2;
-	for(loop1 = 0; loop1 < numsSize; loop1++)
+	for (loop1 = 0; loop1 < numsSize; loop1++)
 	{
 		int temp = target - nums[loop1];
-		for(loop2 = 0; loop2 < numsSize; loop2++)
+		for (loop2 = 0; loop2 < numsSize; loop2++)
 		{
-			if(temp == nums[loop2])
+			if (temp == nums[loop2])
 			{
 				find = 1;
 				break;
 			}
 		}
-		if(1 == find)
+		if (1 == find)
 			break;
 	}
-	if(1 == find)
+	if (1 == find)
 	{
 		result[0] = loop1;
 		result[1] = loop2;
@@ -35,15 +35,16 @@ int* twoSum(int* nums, int numsSize, int target) {
 
 int main(void)
 {
-	int target = 9;
-	int nums[] = {4, 7, 3, 2};
-	int numsSize = sizeof(nums)/sizeof(int);	
+	int target = 55;
+	int nums[] = {1, 5, 12, 33, 43, 22, 4, 7, 3, 2};
+	int numsSize = sizeof(nums) / sizeof(int);
 	int *res;
-	printf("numsSize = %d\n", numsSize);
 
-	res = (int *)malloc(sizeof(int)*2);
+	res = (int *)malloc(sizeof(int) * 2);
 	res = twoSum(nums, numsSize, target);
-
-	printf("nums[%d], nums[%d]\n", res[0], res[1]);
+	if (NULL != res)
+		printf("nums[%d], nums[%d]\n", res[0], res[1]);
+	else
+		printf("target can not be creat.\n");
 	return 0;
 }
