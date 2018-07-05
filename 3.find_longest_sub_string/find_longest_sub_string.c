bcf10b04dc209d92905ae5cd9aa0745b8069fa63
machester <shusheng1991@gmail.com>
2018-07-05 21:02:56 +0800

    not compelte

--- 3.find_longest_sub_string/find_longest_sub_string.c
+++ 3.find_longest_sub_string/find_longest_sub_string.c
@@ -2,61 +2,83 @@
 #include <stdlib.h>
 #include <string.h>
 
-char s[] = "aaweegawegawgaaga";
-
+char s[] = "dvdf";
+//answer is 3
 int lengthOfLongestSubstring(char* s) {
-    int start = 0;
-    int end = 0;
-    int length = 0;
-    int max_sub_string_temp = 0;
-    int max_sub_string = 0;
-    int index = 0;
-    int found = 0;
-    char *str_map = (char *)malloc(sizeof(char) * 30);
+	int start = 0;
+	int end = 0;
+	int length = 0;
+	int max_sub_string_temp = 0;
+	int max_sub_string = 0;
+	int index = 0;
+	int found = 0;
 
-    length = strlen(s);
-    printf("string length = %d\n", length);
+	char str_map[200] = {'\0'};
 
-    if (length == 0)
-        return 0;
-    else if (length == 1)
-        return 1;
+	length = strlen(s);
 
-    for (start = 0; start < length; start++)
-    {
-        str_map[index] = s[start];
-        for (end = start + 1; end < length; end++)
-        {
-            while ((1 != found) || ('\0' != *str_map))
-            {
-                if (s[end] == *str_map) {
-                    found = 1;
-                    break;
-                }
-                str_map++;
-            }
-            printf("str_map = %s", str_map);
-            max_sub_string_temp = end - start;
-            if (found)
-                break;
-        }
-        index++;
-        max_sub_string = max_sub_string < max_sub_string_temp ? max_sub_string_temp : max_sub_string;
-        max_sub_string_temp = 0;
-        printf("------ max_sub_string = %d ----- max_sub_string_temp  %d -------\n", max_sub_string, max_sub_string_temp);
-    }
-    printf("max_sub_string = %d\n", max_sub_string);
-    return max_sub_string;
+	if (length == 0)
+		return 0;
+	else if (length == 1)
+		return 1;
+
+	str_map[0] = s[0];	//init data, first number to be comprated
+	for (start = 0; start < length; start++)
+	{
+
+		for (end = start + 1; end < length; end++)
+		{
+			printf("----- start = %d ---- end = %d ----- index = %d -----\n", start, end, index);
+			int list_index = 0;
+			// copare and find the same word.
+			while ((1 != found) && ('\0' != str_map[list_index]) && (list_index < strlen(str_map))){
+				if (s[end] == str_map[list_index]) {
+					found = 1;
+					break;
+				}
+				else{
+					list_index++;
+				}
+			}
+			if (found){
+				printf("param found : str_map[%d]:%c == s[%d]:%c\n", \
+					list_index, str_map[list_index], end, s[end]);
+				max_sub_string_temp = end - start;
+				//start = end;
+				start = start + 1;
+				end = start;
+				found = 0;
+				list_index = 0;
+			}
+			else{
+
+				index++;
+				str_map[index] = s[end];
+				max_sub_string_temp = end - start + 1;
+				printf("added str_map = %s\n", str_map);
+				printf("max_sub_string_temp = %d\n", max_sub_string_temp);
+
+			}
+			max_sub_string = max_sub_string < max_sub_string_temp ? max_sub_string_temp : max_sub_string;
+			max_sub_string_temp = 0;
+			printf("------ max_sub_string = %d ----- max_sub_string_temp  %d -------\n", max_sub_string, max_sub_string_temp);
+			printf("str_map = %s\n", str_map);
+
+		}
+	}
+	printf("max_sub_string = %d\n", max_sub_string);
+	printf("str_map : %s\n", str_map);
+	return (max_sub_string);
 }
 
 
 int main() {
-    printf("------ start ---------\n");
+	printf("------ start ---------\n");
 
 
-    int length = lengthOfLongestSubstring(s);
+	int length = lengthOfLongestSubstring(s);
 
-    printf("substring length = %d\n", length);
+	printf("substring length = %d\n", length);
 
-    return 0;
+	return 0;
 }
