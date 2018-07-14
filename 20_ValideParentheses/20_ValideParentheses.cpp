55e87cca53edabcbc274220ff88da84f300ff365
machester <shusheng1991@gmail.com>
2018-07-14 16:05:57 +0800

    20

---
+++ 20_ValideParentheses/20_ValideParentheses.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+class Solution{
+public:
+    bool isValiud(string s)
+    {
+        vector <char> cc;
+
+        if (s.empty())
+            return true;
+        else if (0 != s.size() % 2)
+            return false;
+        for(int index = 0; index < s.size(); index++)
+        {
+            if(s[index] == '{' || s[index] == '[' || s[index] == '(')
+                cc.push_back(s[index]);
+            else
+            {
+                if (cc.empty())
+                    return false;
+                if ((s[index] == '}') && (cc[cc.size() - 1] != '{'))
+                    return false;
+                if ((s[index] == ']') && (cc[cc.size() - 1] != '['))
+                    return false;
+                if ((s[index] == ')') && (cc[cc.size() - 1] != '('))
+                    return false;
+                cc.pop_back();
+
+            }
+        }
+        return true;
+    }
+
+};
+int main() {
+    Solution solution;
+    string str("{{{((([[[]]])))}}}");
+    cout << "str is : " << str << endl;
+
+    bool ans = solution.isValiud(str);
+    std::cout << "ans = " << boolalpha << ans << std::endl;
+    return 0;
+}
