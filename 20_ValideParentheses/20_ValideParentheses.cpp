eaa30a0dd6255dc0ec4244bd20835ff70bc0e9d4
machester <shusheng1991@gmail.com>
2018-07-15 12:03:07 +0800

    20

--- 20_ValideParentheses/20_ValideParentheses.cpp
+++ 20_ValideParentheses/20_ValideParentheses.cpp
@@ -4,44 +4,44 @@
 
 using namespace std;
 
-class Solution{
+class Solution {
 public:
-    bool isValiud(string s)
-    {
-        vector <char> cc;
+	bool isValiud(string s) {
+		vector <char> cc;
 
-        if (s.empty())
-            return true;
-        else if (0 != s.size() % 2)
-            return false;
-        for(int index = 0; index < s.size(); index++)
-        {
-            if(s[index] == '{' || s[index] == '[' || s[index] == '(')
-                cc.push_back(s[index]);
-            else
-            {
-                if (cc.empty())
-                    return false;
-                if ((s[index] == '}') && (cc[cc.size() - 1] != '{'))
-                    return false;
-                if ((s[index] == ']') && (cc[cc.size() - 1] != '['))
-                    return false;
-                if ((s[index] == ')') && (cc[cc.size() - 1] != '('))
-                    return false;
-                cc.pop_back();
+		if (s.empty())
+			return true;
+		else if (0 != s.size() % 2)
+			return false;
+		for (int index = 0; index < s.size(); index++) {
+			if (s[index] == '{' || s[index] == '[' || s[index] == '(')
+				cc.push_back(s[index]);
+			else {
+				if (cc.empty())
+					return false;
+				if ((s[index] == '}') && (cc[cc.size() - 1] != '{'))
+					return false;
+				if ((s[index] == ']') && (cc[cc.size() - 1] != '['))
+					return false;
+				if ((s[index] == ')') && (cc[cc.size() - 1] != '('))
+					return false;
+				cc.pop_back();
 
-            }
-        }
-        return true;
-    }
+			}
+		}
+		if (cc.empty)
+			return true;
+		else
+			return true;
+	}
 
 };
 int main() {
-    Solution solution;
-    string str("{{{((([[[]]])))}}}");
-    cout << "str is : " << str << endl;
+	Solution solution;
+	string str("{{{((([[[]]])))}}}");
+	cout << "str is : " << str << endl;
 
-    bool ans = solution.isValiud(str);
-    std::cout << "ans = " << boolalpha << ans << std::endl;
-    return 0;
+	bool ans = solution.isValiud(str);
+	std::cout << "ans = " << boolalpha << ans << std::endl;
+	return 0;
 }
