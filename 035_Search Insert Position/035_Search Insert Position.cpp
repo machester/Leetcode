c7920ea484e97dd9f508b10a712f9741d3e38743
machester <shusheng1991@gmail.com>
2018-07-16 19:23:57 +0800

    035

--- 035_Search Insert Position/035_Search Insert Position.cpp
+++ 035_Search Insert Position/035_Search Insert Position.cpp
@@ -1,17 +1,44 @@
+/**
+ * compare every param in nums arry
+ * Note:
+ *     1. if target < nums[0]
+ *     2. if target > nums[max]
+ *     3. nums is empty
+ */
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
 class Solution {
 public:
-    int searchInsert(vector<int>& nums, int target) 
-    {
-        if(nums.empty())
-            return 0;
-        
-        for(int index = 0; index < nums.size(); index++){
-            if(nums[index] == target)
-                return index;
-            else if((nums[index] <= target) && (nums[index + 1] >= target)){
-                return (index + 1);
-            }
-        }
-        return 0;
-    }
+    int searchInsert(vector<int>& nums, int target);
 };
+
+int main() {
+    
+    //target = 5, target = 1, target = 6, target = 7, target = 0
+    vector<int> nums = {1, 3, 5, 6}; 
+    Solution solution;
+    int retVal = solution.searchInsert(nums, 7);
+    std::cout << "position = " << retVal << endl;
+    return 0;
+}
+
+int Solution::searchInsert(vector<int>& nums, int target)
+{
+    // if nums is empty or nums[0] = target or nums[0] > target, return index 0
+    if(nums.empty() || (target <= nums[0])) 
+        return 0;
+
+    for(int index = 1; index < nums.size(); index++){
+        if(nums[index] == target)
+            return index;
+        else if((nums[index - 1] < target) && (nums[index] > target)){
+            return index;
+        }
+    }
+    if(target > nums[nums.size() - 1])
+        return nums.size();
+    return 0;
+}
