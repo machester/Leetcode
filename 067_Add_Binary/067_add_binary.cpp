4fc873ecde894d190eb13b0ed0086f19b42f6e7d
Jiang Quan <shusheng1991@gmail.com>
2018-12-18 18:09:17 +0800

    067 forget different length string

--- 067_Add_Binary/067_add_binary.cpp
+++ 067_Add_Binary/067_add_binary.cpp
@@ -33,6 +33,116 @@ Output: "10101"
 /**
  * @Description: can accept string like "a ", last word is a divid element
  */
+
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+
+// #include <cstring>
+
+using namespace std;
+
+class Solution {
+public:
+    ~Solution();
+    string addBinary(string a, string b);
+    void show_string(string str);
+};
+
+int main() 
+{
+    // string a = "11001010";
+    // string b = "01111000";
+    string a = "1";
+    string b = "11";
+    string after_operation;
+    
+    Solution solution;
+    solution.show_string(a);
+    solution.show_string(b);
+    after_operation = solution.addBinary(a, b);
+    solution.show_string(after_operation);
+    return 0;
+}
+
+void Solution::show_string(string str)
+{
+    cout << "string is : " << str << endl;
+#if 0 // for vector start
+    cout << "item = { ";
+    for(string::iterator item = digits.begin(); item < digits.end(); item++) {
+        cout << *item << " ";
+    }
+    cout << " }" << endl;
+#endif  // for vector end
+}
+
+string Solution::addBinary(string a, string b)
+{
+    cout << "------------- start ---------" << endl;
+    string ret;
+    int temp;
+    int carry_mark = 0;
+    
+    if (a.empty() && b.empty())
+        return a;
+        
+    int length = (a.length() >= b.length()) ? a.length() : b.length();
+    cout << "max length = " << length << endl;
+    for(int index = length - 1; index >= 0; --index) {
+        if (!a.empty() && !b.empty()) {
+            temp = (int)(a[index] - '0') + (int)(b[index] - '0');
+            
+        }
+        
+        if(temp > 1 && carry_mark == 0) {
+            temp = 0;
+            carry_mark = 1;
+        } else if(1 == carry_mark && temp < 1){
+            temp += carry_mark;
+            carry_mark = 0;
+        } else if(temp > 1 && carry_mark == 1) {
+            temp = 1; 
+            carry_mark = 1;
+        }
+        else if(temp == 1 && carry_mark == 1) {
+            temp = 0; 
+            carry_mark = 1;
+        }
+        cout << "loop " << length - index << ": a[" << index << "] = " << a[index] \
+                << ",  b[" << index << "] = " << b[index] \
+                << ", temp = " << temp << ", carry_mark = " << carry_mark
+                << endl;
+        if (0 != temp)
+
+            ret.push_back('1');
+        else 
+            ret.push_back('0');
+        
+        // clean 
+        a.pop_back();
+        b.pop_back();
+    }
+    if(1 == carry_mark) {
+        cout << "carry_mark = " << carry_mark << endl;
+        ret.push_back('1');
+    }
+    // inver string
+    reverse(ret.begin(), ret.end());
+    return ret;
+}
+
+Solution::~Solution() 
+{
+    cout << "------------- end ---------" << endl;    
+}
+
+
+
+
+
+#if 0
 #include <iostream>
 #include <vector>
 #include <string>
@@ -64,13 +174,6 @@ int main()
 void Solution::show_string(string str)
 {
     cout << "string is : " << str << endl;
-#if 0 // for vector start
-    cout << "item = { ";
-    for(string::iterator item = digits.begin(); item < digits.end(); item++) {
-        cout << *item << " ";
-    }
-    cout << " }" << endl;
-#endif  // for vector end
 }
 
 string Solution::addBinary(string a, string b)
@@ -116,3 +219,4 @@ Solution::~Solution()
 {
     cout << "------------- end ---------" << endl;    
 }
+#endif 
---
+++ 067_Add_Binary/067_c_add_binary.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <string.h>
+
+
+
+char* addBinary(char* a, char* b) {
+    
+}
