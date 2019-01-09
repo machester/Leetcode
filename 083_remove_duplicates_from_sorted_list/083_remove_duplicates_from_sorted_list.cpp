839184659074b0ce9b6b1fc67a58f7f1ed461828
Jiang Quan <shusheng1991@gmail.com>
2019-01-09 16:18:54 +0800

    083 completed list and duplicated node remove

--- 083_remove_duplicates_from_sorted_list/083_remove_duplicates_from_sorted_list.cpp
+++ 083_remove_duplicates_from_sorted_list/083_remove_duplicates_from_sorted_list.cpp
@@ -31,71 +31,159 @@ public:
 #include <iostream>
 #include <vector>
 #include <string>
-#include <algorithm>
 
 // #include <cstring>
 
-using namespace std;
 
-
- struct ListNode {
+ typedef struct ListNode {
      int val;
-     ListNode *next;
- };
+     struct ListNode *next;
+ }list_node;
 
 class Solution {
 public:
     ~Solution();
-    ListNode* deleteDuplicates(ListNode* head);
-    void show_string(ListNode *str);
-    void remove_current_node(ListNode *prev, ListNode *next);
-    void ListNode * init_list(ListNode * var);
+    list_node * deleteDuplicates(list_node *head);
     
-
+    void show_list(list_node *lls);
+    void remove_current_node(list_node *prev, list_node *next);
+    list_node * list_head_init(void);
+    list_node * list_create_node(void);
+    void set_current_node_val(list_node * node, int value);
+    void add_node_tail(list_node *head, list_node *new_node);
+    void add_node_head(list_node *head, list_node *new_node);
 };
 
 int main() 
 {
-    int x = 8192;
+    std::cout << "------------- start ---------" << std::endl;
     Solution solution;
-    struct ListNode * lls = nullptr;
-    // struct ListNode * ls = new struct ListNode;
+    list_node * lls = solution.list_head_init();
+    solution.set_current_node_val(lls, 1);
     
-    return 0;
-}
-
-void Solution::show_string(ListNode *str)
-{
-    cout << "string is : " << end;
-    for(ListNode *item = str; item != null; item = item ->next) {
-    	cout << "---> " << item->val
-		<< ", " << end;
+    
+    // init sorted list
+    // list_node * node_ptr;
+    // for(int loop = 2; loop < 10; loop++) {
+    //     node_ptr = solution.list_create_node();
+    //     solution.set_current_node_val(node_ptr, loop);
+    //     solution.add_node_tail(lls, node_ptr);
+    // }
+    
+    // init duplicates sorted list
+    list_node * node_ptr;
+    for(int loop = 2; loop < 10; loop++) {
+        node_ptr = solution.list_create_node();
+        solution.set_current_node_val(node_ptr, loop);
+        solution.add_node_tail(lls, node_ptr);
+        if(loop % 2 != 0) {
+            node_ptr = solution.list_create_node();
+            solution.set_current_node_val(node_ptr, loop);
+            solution.add_node_tail(lls, node_ptr);
+        }
     }
-    cout << endl;
-}
-
-void Solution::remove_current_node(ListNode *prev, ListNode *next)
-{
-	
-	prev->next = next;
-}
     
-ListNode* Solution::deleteDuplicates(ListNode* head)
-{
-    cout << "------------- start ---------" << endl;
-    int retval;
     
-    return retval;    
-}
+    
+    
+    solution.show_list(lls);
 
-void ListNode * init_list(ListNode * var)
-{
-	newList = new ListNode();
-
+    /*---------------- delete duplicated node -------------------*/
+    std::cout << "------------- delete duplicated node ------------" << std::endl;    
+    solution.deleteDuplicates(lls);
+    std::cout << "------------- finished clean duplicated list ------------" << std::endl; 
+    solution.show_list(lls);
+    return 0;
 }
 
 Solution::~Solution() 
 {
-    cout << "------------- end ---------" << endl;    
+    std::cout << "------------- end ---------" << std::endl;    
 }
 
+list_node * Solution::deleteDuplicates(list_node * head)
+{   
+    if(head == nullptr || head->next == nullptr)
+        return head;
+    list_node * free_ptr = nullptr;
+    list_node * prev_node = head;
+    list_node * curr_node = head->next;
+    
+    while(curr_node != nullptr) {
+        if(prev_node->val == curr_node->val) {
+
+            if(nullptr == curr_node->next) {        // current node is the last node
+                std::cout << "last node curr_node-> val = " << curr_node->val << std::endl;
+                prev_node->next = nullptr;
+                delete curr_node;
+                curr_node = nullptr;            
+            } else {            // current node not the last node
+                free_ptr = curr_node;
+                curr_node = curr_node->next;
+                std::cout << "free_ptr -> val = " << free_ptr->val << std::endl;
+                delete free_ptr;
+                free_ptr = nullptr;
+            }
+        }
+        if(curr_node != nullptr) {      // make sure if current node is last one and deleted
+            // link node make sure is still a linked list. if delete this can make it linked
+            prev_node->next = curr_node;
+            // move prev_node and current node ptr
+            prev_node = curr_node;
+            curr_node = curr_node->next;
+        }
+        Solution::show_list(head);
+    }
+    return head;    
+}
+
+void Solution::show_list(list_node *lls)
+{
+    std::cout << "string is : " << std::endl;
+    for(list_node *node = lls; node != nullptr; node = node->next) {
+    	std::cout << "---> " << node->val << ", ";
+    }
+    std::cout << std::endl;
+}
+
+void Solution::remove_current_node(list_node *prev, list_node *next)
+{
+	prev->next = next->next;
+}
+
+list_node * Solution::list_head_init(void)
+{
+	list_node * newList = new list_node();
+    newList->next = nullptr;
+    return newList; 
+}
+
+list_node * Solution::list_create_node(void)
+{
+    list_node * new_node = new list_node();
+    new_node->next = nullptr;
+    return new_node; 
+}
+
+
+
+void Solution::add_node_tail(list_node *head, list_node *new_node)
+{
+    while(head->next != nullptr) {
+        head = head->next;
+    }
+    head ->next = new_node;
+    new_node ->next = nullptr;
+}
+
+void Solution::add_node_head(list_node *head, list_node *new_node)
+{
+    list_node * ptr = head;
+    new_node ->next = ptr;
+    head->next = new_node;
+}
+
+void Solution::set_current_node_val(list_node * node, int value)
+{
+    node->val = value;
+}
